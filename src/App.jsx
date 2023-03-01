1286ca0e8b44aa7d41ffa180dd2b3cf77e72f3be
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-03-01 14:08:11 -0500

    FEAT: add breakpoits and make responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import data from "./data";
 const App = () => {
   const [summarys, setSummarys] = useState(data)
   return (
-    <div className='w-screen h-screen flex justify-center sm:items-center'>
-      <div className='sm:w-5/12 sm:flex '>
+    <div className='w-screen h-screen sm:flex justify-center items-center'>
+      <div className='grid sm:grid-cols-2 sm:items-stretch bg-white drop-shadow-xl md:w-10/12 sm:rounded-3xl lg:w-8/12 xl:w-7/12 2xl:w-5/12'>
         <Result />
         <SummaryList summarys={summarys} />
       </div>
--- src/components/SummaryList.jsx
+++ src/components/SummaryList.jsx
@@ -3,15 +3,15 @@ import Card from "./Card";
 
 const SummaryList = ({ summarys }) => {
   return (
-    <div className='w-10/12 m-auto mt-8 sm:shadow-2xl sm:rounded-r-2xl sm:p-5 sm:mt-0 sm:bg-white'>
-      <p className='mb-4'>Summary</p>
-      <div className='flex flex-col gap-5'>
+    <div className='m-auto mt-8 sm:mt-0 w-full p-3 sm:rounded-r-3xl'>
+      <p p className='mb-4'>Summary</p>
+      <div className='flex flex-col gap-3'>
         {
           summarys.map(summary => <Card key={summary.category} summary={summary} />)
         }
       </div>
       <button className='btn p-4 mt-7 rounded-full'>Continue</button>
-    </div>
+    </div >
   )
 }
 
